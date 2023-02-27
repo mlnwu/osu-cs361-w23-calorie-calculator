90a4fa1d78bd500a29bba75cbf64e7a60aa3eb46
wumag <107904188+wumag@users.noreply.github.com>
2023-02-26 16:03:28 -0800

    edit comments

--- calorie_calculator_client.py
+++ calorie_calculator_client.py
@@ -7,22 +7,20 @@ import zmq
 import time
 import os
 
-# Define a lambda function that clears the console
+# Lambda function that clears the console
 clear = lambda: os.system('clear')
 
-# Create a ZeroMQ context object
 context = zmq.Context()
 
-# Print a message indicating connection to Calorie Calculator and clear the console after a 5 second delay
 print("Connecting to Calorie Calculator...")
 time.sleep(5)
 clear()
 
-# Create a ZeroMQ socket object and connect to the Calorie Calculator service
+# ZeroMQ socket object connects to the Calorie Calculator service
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:6666")
 
-# Read the path from a text file, get the filename and extension, and create a list of requests to send to the service
+# Read the path from a text file and get the filename and extension
 with open('request.txt', 'r') as f:
     path = f.read().strip()
 filename = os.path.basename(path)
@@ -35,7 +33,6 @@ requests = ["{}{} {}".format(name_without_extension, extension, "gender"),
             "{}{} {}".format(name_without_extension, extension, "activity"),
             "{}{} {}".format(name_without_extension, extension, "calories")]
 
-# Iterate through each request and send it to the Calorie Calculator service, then print the response
 for request in requests:
     print(f"Sending request '{request}'...")
     socket.send_string(request)
